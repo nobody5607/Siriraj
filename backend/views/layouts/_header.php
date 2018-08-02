4ce518507e419b2e74a4907d6376e67d4fca2925
chanpan <chanpan.nuttaphon@gmail.com>
2018-08-02 15:57:52 +0700

    header

--- backend/views/layouts/_header.php
+++ backend/views/layouts/_header.php
@@ -10,7 +10,7 @@ Yii::$app->name = 'Siriraj';
 /* @var $this \yii\web\View */
 ?>
 <header class="main-header">
-    <?= Html::a('<span class="logo-mini">APP</span><span class="logo-lg"><i class="fa fa-diamond"></i> ' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
+    <?= Html::a('<span class="logo-mini"><i class="fa fa-diamond"></i></span><span class="logo-lg"><i class="fa fa-diamond"></i> ' . Yii::$app->name . '</span>', Yii::$app->homeUrl, ['class' => 'logo']) ?>
     <nav class="navbar navbar-static-top" role="navigation">
         <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
             <span class="sr-only">Toggle navigation</span>
