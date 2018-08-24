2f3af10aee6bfdbb69db788015f28dbaf0684c16
chanpan <chanpan.nuttaphon@gmail.com>
2018-08-25 00:53:18 +0700

    fix

--- backend/views/site/test.php
+++ backend/views/site/test.php
@@ -1,2 +1,14 @@
-<?= phpinfo()
-?>
+<form enctype="multipart/form-data" action="upload.php" method="post">
+    <input name="file[]" type="file" />
+    <button class="add_more">Add More Files</button>
+    <input type="button" value="Upload File" id="upload"/>
+</form>
+
+<?php \richardfan\widget\JSRegister::begin();?>
+<script>
+    $('.add_more').click(function(e){
+        e.preventDefault();
+        $(this).before("<input name='file[]' type='file'/>");
+    });
+</script>
+<?php \richardfan\widget\JSRegister::end();?>
