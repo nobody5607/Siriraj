e7d72df770ee2036eb005287e98f81b8d174f959
chanpan <chanpan.nuttaphon@gmail.com>
2018-08-27 11:52:12 +0700

    frontend

--- frontend/modules/sections/views/content-management/_view-data-content-item.php
+++ frontend/modules/sections/views/content-management/_view-data-content-item.php
@@ -1,71 +1,46 @@
 <?php 
     use yii\helpers\Html;
-     
-    if($model['file_type'] == '2'){
-        //image
-        $link  = "";        
-        $link .= Html::img("{$model['file_path']}/{$model['file_name']}", 
-                [
-                    'class'=>'img img-responsive img-rounded',
-                    'style'=>'width:100px;height: 100px;'
-                ]);
-        $name_str = backend\modules\sections\classes\JFiles::lengthName($model['file_name_org']);        
-        $link .= "<div>{$name_str}</div>";
-    }else if($model['file_type'] == '3'){
-        //video
-        $link = "";
-        $link .= "
-             <video style='width:100%;height: 100px;' controls>
-                <source src='{$model['file_path']}/{$model['file_name']}' type='video/mp4'>                 
-                Your browser does not support the video tag.
-            </video>
-        ";
-        $name_str = backend\modules\sections\classes\JFiles::lengthName($model['file_name_org']);        
-        $link .= "<div>{$name_str}</div>";
-    }else if($model['file_type'] == '4'){
-        //audio
-        $link = "";
-        $link .= "            
-            <audio controls style='width:100%;height: 100px;'>
-                <source src='{$model['file_path']}/{$model['file_name']}' type='audio/mpeg'>                 
-                Your browser does not support the video tag.
-            </audio>
-        ";
-       $name_str = backend\modules\sections\classes\JFiles::lengthName($model['file_name_org']);        
-        $link .= "<div>{$name_str}</div>";  
-    }else if($model['file_type'] == '6'){
-        //image
-        $link  = "";        
-        $link .= Html::img("{$model['file_path']}/{$model['file_name']}", 
+?>
+    
+<a href="<?= "/sections/content-management/view-file?content_id={$_GET['content_id']}&file_id={$model['id']}&filet_id={$model['file_type']}"?>" style="margin-top: 5px;" href=""   data-id="<?= $model['id']?>" id="<?= "btn-{$model['id']}"?>" data-action="view-file" class="content-popup btnCall text-left">
+    <?php
+        if($model['file_type'] == '2'){
+            echo Html::img("{$model['file_path']}/thumbnail/{$model['file_name']}", 
+                    [
+                        'class'=>'img img-responsive img-rounded',
+                        'style'=>''
+            ]);            
+        }else if($model['file_type'] == '3'){
+            echo "
+                <video style='width:100%;height: 100px;' controls>
+                    <source src='{$model['file_path']}/{$model['file_name']}' type='video/mp4'>                 
+                    Your browser does not support the video tag.
+                </video>
+            ";         
+        }else if($model['file_type'] == '4'){
+            echo "            
+                <audio controls style='width:100%;height: 100px;'>
+                    <source src='{$model['file_path']}/{$model['file_name']}' type='audio/mpeg'>                 
+                    Your browser does not support the video tag.
+                </audio>
+                ";
+        }else if($model['file_type'] == '6'){
+            echo Html::img("{$model['file_path']}/{$model['file_name']}", 
                 [
                     'class'=>'img img-responsive img-rounded',
                     'style'=>'width:100px;height: 80px;'
                 ]);
-        $name_str = backend\modules\sections\classes\JFiles::lengthName($model['file_name_org']);
-        $link .= "<div>{$name_str}</div>";
-    }
-    else{
-        $name_str = backend\modules\sections\classes\JFiles::lengthName($model['file_name_org']);
-          
-       $link= " 
-            <div><i class='fa fa-file'></i> {$name_str}</div>
-        ";   
-    }
-    $taga = "";
-   
-    $taga .=  Html::a($link,"/sections/content-management/view-file?content_id={$_GET['content_id']}&file_id={$model['id']}&filet_id={$model['file_type']}" ,
-    //['/knowledges/content/view-content-data','content_id'=>$_GET['content_id'], 'file_id'=>$model['id'], 'filet_id'=>$model['file_type']], 
-    [
-        'id'=>"btn-{$model['id']}",
-        'data-action'=>'view-file',
-        'class'=>'content-popup btnCall',
-        'data-id'=>$model['id'],
-                'title'=>$model['file_name_org'],
-        'data-url'=>"/sections/content-management/view-file?content_id={$_GET['content_id']}&file_id={$model['id']}&filet_id={$model['file_type']}"
-    ]);
-    echo $taga;
-?> 
-<?php $this->registerCss("a{color:#000;}")?>
+        }else{
+            
+        }
+        $name_str = backend\modules\sections\classes\JFiles::lengthName($model['file_name_org'] , 20);
+        echo "<div title='{$model['file_name_org']}' >{$name_str}</div>";
+    ?>
+</a>
+
+
+ 
+<?php $this->registerCss("a{color:#000;}")?> 
 <?php 
     $modal = "modal-contents";
 ?>
