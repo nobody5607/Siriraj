a2f85cf6d2325352f11aa65bd3e324f17264b7ad
chanpan <chanpan.nuttaphon@gmail.com>
2018-09-03 10:00:14 +0700

    profile

---
+++ frontend/messages/en/sitecode.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'Create' => 'Create',
+    'Update' => 'Update',
+    'Cancel' => 'Cancel',
+    'View' => 'View',
+    'Manage' => 'Manage',
+    'List' => 'List',
+    'New' => 'New',
+    'Delete' => 'Delete',
+    'Site name'=>'Site name',
+    'Site id'=>'Site id',
+    'Sitecode'=>'Site Codes',
+    'Site Code'=>'Site Codes',
+];
