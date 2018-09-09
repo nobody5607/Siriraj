ca424ecb7a4d54f5387513ada90223d8525147a5
chanpan <chanpan.nuttaphon@gmail.com>
2018-09-09 23:51:04 +0700

    preview

--- frontend/modules/sections/views/order/send-mail.php
+++ frontend/modules/sections/views/order/send-mail.php
@@ -15,7 +15,7 @@ $email_default = \backend\modules\cores\classes\CoreOption::getParams('email_req
         <div class="panel panel-default">
             <div class="panel-heading">
                 <i class="fa fa-envelope"></i> <?= Html::encode($this->title) ?>
-                <div class="pull-right"><a class="btn btn-sm btn-primary" target="_BLANK" href="/sections/order/print?id==<?= Yii::$app->request->get('id')?>&type=print"><?= Yii::t('section','Preview Form')?></a></div>
+                <div class="pull-right"><a class="btn btn-sm btn-primary" target="_BLANK" href="/sections/order/print?id==<?= Yii::$app->request->get('id')?>&type=preview"><?= Yii::t('section','Preview Form')?></a></div>
             </div>
             <div class="panel-body">
                 <?php 
