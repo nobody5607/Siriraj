b166e0e75a9e61c685bd0f15f77c395a35252952
chanpan <chanpan.nuttaphon@gmail.com>
2018-09-16 22:55:37 +0700

    fix

--- backend/modules/sections/views/content-management/view-file/_item.php
+++ backend/modules/sections/views/content-management/view-file/_item.php
@@ -19,18 +19,17 @@
 <a href="<?= "/sections/content-management/view-file?content_id={$_GET['content_id']}&file_id={$model['id']}&filet_id={$model['file_type']}"?>" style="margin-top: 5px;" href=""   data-id="<?= $model['id']?>" id="<?= "btn-{$model['id']}"?>" data-action="view-file" class="content-popup btnCall text-left">
     <?php
         if($model['file_type'] == '2'){
-            echo Html::img("{$model['file_path']}/thumbnail/{$model['file_name']}", 
-                    [
-                        'class'=>'img img-responsive img-rounded',
-                        'style'=>''
+            echo Html::img("{$model['file_path']}/thumbnail/{$model['file_view']}", [
+                'class' => 'img img-responsive img-rounded',
+                'style' => 'height:120px;    margin: 0 auto;'
             ]);            
         }else if($model['file_type'] == '3'){
              
-            if(isset($model['file_thumbnail'])){
+            if(isset($model['file_thumbnail']) || isset($model['file_thumbnail']) != ""){
                 echo "<img src='{$model['file_thumbnail']}'>";
             }else{
                 echo "            
-                    <div style='font-size: 50pt;text-align: center;padding-top: 15px;'><i class='fa fa-file-video-o'></i></div>
+                    <div style='font-size: 45pt;text-align: center;padding-top: 15px;'><i class='fa fa-file-video-o'></i></div>
                 ";   
             }
                 
@@ -38,12 +37,23 @@
             echo "            
                 <div style='font-size: 50pt;text-align: center;padding-top: 15px;'><i class='fa fa-music'></i></div>
             ";
-        }else if($model['file_type'] == '6'){
-            echo Html::img("{$model['file_path']}/{$model['file_name']}", 
-                [
-                    'class'=>'img img-responsive img-rounded',
-                    'style'=>'width:100px;height: 80px;'
-                ]);
+        }else if ($model['file_type'] == 5 || $model['file_type'] == 6 || $model['file_type'] == 7){
+            $fileNameStr = explode(".", $model['file_name']);
+            $icon = "";
+            if ($fileNameStr[1] == 'doc' || $fileNameStr[1] == 'docx') {
+                $icon = "<i class='fa fa-file-word-o'></i>";
+            } else if ($fileNameStr[1] == 'ppt' || $fileNameStr[1] == 'pptx') {
+                $icon = "<i class='fa fa-file-powerpoint-o'></i>";
+            } else if ($fileNameStr[1] == 'xls' || $fileNameStr[1] == 'xlsx') {
+                $icon = "<i class='fa fa-file-excel-o'></i>";
+            } else if ($fileNameStr[1] == 'pdf') {
+                $icon = "<i class='fa fa-file-pdf-o'></i>";
+            } else if ($fileNameStr[1] == 'zip' || $fileNameStr[1] == 'rar') {
+                $icon = "<i class='fa fa-file-pdf-o'></i>";
+            } else {
+                $icon = "<i class='fa fa-file-archive-o'></i>";
+            }
+            echo "<div style='font-size: 45pt;text-align: center;padding-top: 15px;'>{$icon}</div>";
         }else{
             
         }
