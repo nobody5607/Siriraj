dacbf74708f332ed939613b7e6514e66d8d22690
chanpan <chanpan.nuttaphon@gmail.com>
2018-09-17 21:08:55 +0700

    detail

--- frontend/modules/sections/views/content-management/view-file/_view-file-right.php
+++ frontend/modules/sections/views/content-management/view-file/_view-file-right.php
@@ -10,7 +10,7 @@ use yii\helpers\Html;
                 <i class="fa fa-info-circle" aria-hidden="true"></i> <?= Yii::t('section','Note')?>
             </label>
             <div class="border-bottom">
-                <div style="word-wrap:break-word;"><?= $dataDefault['description'] ?></div>
+                <div style="word-wrap:break-word;"><?= $dataDefault['details'] ?></div>
             </div>     
             <div class="border-bottom">
                 <label>
