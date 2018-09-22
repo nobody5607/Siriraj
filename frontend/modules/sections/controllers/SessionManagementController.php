77fc0ec3e3c6a5144564c203375448008b604635
chanpan <chanpan.nuttaphon@gmail.com>
2018-09-22 22:15:18 +0700

    search

--- frontend/modules/sections/controllers/SessionManagementController.php
+++ frontend/modules/sections/controllers/SessionManagementController.php
@@ -94,7 +94,8 @@ class SessionManagementController extends Controller
         $txtsearch = Yii::$app->request->get('txtsearch', '');
         $fileType = \common\models\FileType::findOne($type_id);
         $data = \common\models\Files::find();
-        $model = $data->where('description LIKE :description  OR detail_meta LIKE :detail_meta  OR file_name_org LIKE :file_name OR meta_text LIKE :meta_text',[
+        $model = $data->where('details LIKE :details OR description LIKE :description  OR detail_meta LIKE :detail_meta  OR file_name_org LIKE :file_name OR meta_text LIKE :meta_text',[
+            ':details'=>"%{$txtsearch}%",
             ':description'=>"%{$txtsearch}%",
             ':detail_meta'=>"%{$txtsearch}%",
             ':file_name'=>"%{$txtsearch}%",
